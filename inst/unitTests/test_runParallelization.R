6a60ac396c50543ee5dde92c58f1a35806729b9c
Asier Ortega Legarreta <asierortega20@gmail.com>
2024-02-07 08:41:38 +0100

    timeout fixed

--- inst/unitTests/test_runParallelization.R
+++ inst/unitTests/test_runParallelization.R
@@ -1,12 +1,11 @@
 ##########################################################
-# Running the pipeline with and without parallelization
+# Running the pipeline with parallelization
 ##########################################################
 require("ClustAll")
 data("BreastCancerWisconsin", package = "ClustAll") # load example data
-data("BreastCancerWisconsinMISSING", package = "ClustAll") # load example data
 
 test_pipeline <- function() {
-  obj <- createClustAll(wdbcNA, nImputation = 3,  colValidation = "Diagnosis")
+  obj <- createClustAll(data = wdbc, colValidation = "Diagnosis",
+                        nImputation = NULL, dataImputed = NULL)
   runClustAll(obj, threads = 2, simplify = TRUE) # parallelization
-  runClustAll(obj, threads = 1, simplify = FALSE)
 }
